3dc5e130b15c750a020351cecd2153261bdd2321
muratvuranok <mrtvrnk@gmail.com>
2019-07-18 12:32:30 +0300

    Sayısal Loto Örneği Eklendi

--- WFA_Odevler/Form1.Designer.cs
+++ WFA_Odevler/Form1.Designer.cs
@@ -28,13 +28,126 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.t4 = new System.Windows.Forms.TextBox();
+            this.t5 = new System.Windows.Forms.TextBox();
+            this.t6 = new System.Windows.Forms.TextBox();
+            this.t1 = new System.Windows.Forms.TextBox();
+            this.t2 = new System.Windows.Forms.TextBox();
+            this.t3 = new System.Windows.Forms.TextBox();
+            this.btnCekilisYap = new MetroFramework.Controls.MetroButton();
+            this.SuspendLayout();
+            // 
+            // t4
+            // 
+            this.t4.AcceptsTab = true;
+            this.t4.Enabled = false;
+            this.t4.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.t4.Location = new System.Drawing.Point(410, 82);
+            this.t4.Multiline = true;
+            this.t4.Name = "t4";
+            this.t4.Size = new System.Drawing.Size(123, 96);
+            this.t4.TabIndex = 0;
+            this.t4.Text = "0";
+            this.t4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // t5
+            // 
+            this.t5.Enabled = false;
+            this.t5.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.t5.Location = new System.Drawing.Point(539, 82);
+            this.t5.Multiline = true;
+            this.t5.Name = "t5";
+            this.t5.Size = new System.Drawing.Size(123, 96);
+            this.t5.TabIndex = 0;
+            this.t5.Text = "0";
+            this.t5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // t6
+            // 
+            this.t6.Enabled = false;
+            this.t6.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.t6.Location = new System.Drawing.Point(668, 82);
+            this.t6.Multiline = true;
+            this.t6.Name = "t6";
+            this.t6.Size = new System.Drawing.Size(123, 96);
+            this.t6.TabIndex = 0;
+            this.t6.Text = "0";
+            this.t6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // t1
+            // 
+            this.t1.Enabled = false;
+            this.t1.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.t1.Location = new System.Drawing.Point(23, 82);
+            this.t1.Multiline = true;
+            this.t1.Name = "t1";
+            this.t1.Size = new System.Drawing.Size(123, 96);
+            this.t1.TabIndex = 0;
+            this.t1.Text = "0";
+            this.t1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // t2
+            // 
+            this.t2.Enabled = false;
+            this.t2.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.t2.Location = new System.Drawing.Point(152, 82);
+            this.t2.Multiline = true;
+            this.t2.Name = "t2";
+            this.t2.Size = new System.Drawing.Size(123, 96);
+            this.t2.TabIndex = 0;
+            this.t2.Text = "0";
+            this.t2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // t3
+            // 
+            this.t3.Enabled = false;
+            this.t3.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.t3.Location = new System.Drawing.Point(281, 82);
+            this.t3.Multiline = true;
+            this.t3.Name = "t3";
+            this.t3.Size = new System.Drawing.Size(123, 96);
+            this.t3.TabIndex = 0;
+            this.t3.Text = "0";
+            this.t3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnCekilisYap
+            // 
+            this.btnCekilisYap.FontSize = MetroFramework.MetroButtonSize.Tall;
+            this.btnCekilisYap.Location = new System.Drawing.Point(23, 205);
+            this.btnCekilisYap.Name = "btnCekilisYap";
+            this.btnCekilisYap.Size = new System.Drawing.Size(768, 64);
+            this.btnCekilisYap.TabIndex = 1;
+            this.btnCekilisYap.Text = "Çekiliş Yap";
+            this.btnCekilisYap.UseSelectable = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(819, 292);
+            this.Controls.Add(this.btnCekilisYap);
+            this.Controls.Add(this.t3);
+            this.Controls.Add(this.t2);
+            this.Controls.Add(this.t6);
+            this.Controls.Add(this.t1);
+            this.Controls.Add(this.t5);
+            this.Controls.Add(this.t4);
+            this.Name = "Form1";
+            this.Text = "SAYISAL LOTO - 6 / 49";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox t4;
+        private System.Windows.Forms.TextBox t5;
+        private System.Windows.Forms.TextBox t6;
+        private System.Windows.Forms.TextBox t1;
+        private System.Windows.Forms.TextBox t2;
+        private System.Windows.Forms.TextBox t3;
+        private MetroFramework.Controls.MetroButton btnCekilisYap;
     }
 }
 
